c25f6ff6e09adae73237835b9353028b02e23304
Gabrielcavalcantebelo <101519095+Gabrielcavalcantebelo@users.noreply.github.com>
2022-05-11 16:58:28 -0300

    Add files via upload

---
+++ PrimeiroPrograma.java
@@ -0,0 +1,33 @@
+package fundamentos;
+/**
+ * Essa Classe representa... JAVADOC
+ * 
+ *@author Gabriel Belo<@gmail.com>
+ * @sice JDK1.0
+ *@see
+ */ 
+public class PrimeiroPrograma {
+/**
+ * 	
+ * @param args
+ */
+public static void main(String[] args) {   
+
+	//uma sentenca de codigo termina com ;
+	//mais um comentario...
+	//Fim
+	System.out.println("Primeiro programa #01");          
+	
+    /*
+     * linha 1
+     * linha 2
+     * linha 3...
+     */
+      System.out.println("Primeiro programa #02");           
+     
+      System.out.println("fim"); // Aqui tb funciona!
+      
+     
+   }   
+}
+		         
